2a87b46844b024c3165c53a5ebf5f7a6249e1efa
Jonty <89488176+Jomlom@users.noreply.github.com>
2026-06-26 00:00:38 +0100

    make components.js site wide

--- components.js
+++ components.js
@@ -1,4 +1,19 @@
-// site-nav web component
+class HomeNav extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      <nav>
+        <span class="nav-logo">JA<span class="cursor-blink">_</span></span>
+        <div class="nav-links">
+          <a href="projects/">Projects</a>
+          <a href="background/">Background</a>
+          <a href="log/">Log</a>
+          <a href="#contact">Contact</a>
+        </div>
+      </nav>
+    `
+  }
+}
+
 class SiteNav extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
@@ -15,16 +30,17 @@ class SiteNav extends HTMLElement {
   }
 }
 
-// site-footer web component
 class SiteFooter extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <footer>
         <span>Jonty Ali · Durham · 2026</span>
+        <span>© 2026 Jonty Ali. All rights reserved.</span>
       </footer>
     `
   }
 }
 
+customElements.define('home-nav', HomeNav)
 customElements.define('site-nav', SiteNav)
 customElements.define('site-footer', SiteFooter)
